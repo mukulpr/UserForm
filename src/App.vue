2fe602733e67ae18a1b90c9b79630054bdbeedb6
mukulpr <mukul.parashar@vidyamantra.com>
2022-06-02 10:40:02 +0530

    Added Files SuccessFully

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <the-form></the-form>
+</template>
+<script>
+import TheForm from './components/TheForm.vue'
+export default{
+  components:{'the-form':TheForm}
+}
+</script>
+
+
+
+<style>
+
+</style>
---
+++ src/components/RatingControl.vue
@@ -0,0 +1,29 @@
+<template>
+    <ul>
+        <li><button>Poor</button></li>
+        <li><button>Average</button></li>
+        <li><button>Button</button></li>
+    </ul>
+</template>
+<style scoped>
+ul{
+    list-style: none;
+    margin: 0.5rem;
+    padding: 0;
+    display: flex;
+}
+li{
+    margin: 0 1rem;
+    border: 1px solid #ccc;
+    padding: 1rem;
+    display: flex;
+    justify-content: center;
+    align-items: center
+}
+button{
+    font: inherit;
+    border: none;
+    background-color: transparent;
+    cursor: pointer;
+}
+</style>
